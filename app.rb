35c739de3ef71e5a287201029bc0d74fa1128a75
Anditto Heristyo <anditto.heristyo@gmail.com>
2017-02-28 13:28:09 -0800

    Add dummy data for demo

--- app.rb
+++ app.rb
@@ -88,21 +88,24 @@ get '/pointrequest' do
 
   # Dummy data
   requestexchange = {"request": {"suica":200}, "offer":"ANA"}
-  response = {"fee":15, "assetid":"ANA", "cost":400, "tx":"0000132...."}
+  response = {"fee":15, "assetid":"ANA", "cost":400, "tx":"0100000001a0f9836b4ba55bccb6ebf9e199899d446e532008784cf439d24bd82cd1c182ab0100000000ffffffff02016410bc6730219f75a4580874734cbed00d8bef65e5908991a264c351d2931ee00100000002540be4001976a914d4391f69c20725f7811f44acc062107c2c94e74b88ac01e5ca390fef3fc33c29a8bfa803a612a3819df7015bdd953d0a4e994f844084010100000009502f90001976a914a1798cc6ee314d3daa4d530c3da7dd9bb5b10f4788ac00000000"}
+  post_data = ""
+  response.each do |k,v|
+    post_data = post_data + k.to_s + "=" + v.to_s + "&"
+  end
 
   # Redirect on a success
-  #redirect '/confirm?response='+response["tx"].to_s
+  redirect '/confirm?' + post_data
   erb :pointrequest
 end
 
 # /transaction?response=reponse
 get '/confirm' do
   ### Expecting to receive:
-  # {"fee":15, "assetid":"ANA", "cost":400, "tx":"0000132...."}
-  response = params['response']
+  # {"fee":15, "assetid":"ANA", "cost":400, "tx":"0100000001a0f9836b4ba55bccb6ebf9e199899d446e532008784cf439d24bd82cd1c182ab0100000000ffffffff02016410bc6730219f75a4580874734cbed00d8bef65e5908991a264c351d2931ee00100000002540be4001976a914d4391f69c20725f7811f44acc062107c2c94e74b88ac01e5ca390fef3fc33c29a8bfa803a612a3819df7015bdd953d0a4e994f844084010100000009502f90001976a914a1798cc6ee314d3daa4d530c3da7dd9bb5b10f4788ac00000000"}
 
   alice = BitcoinRPC.new('http://user:password@localhost:10000')
-  receivedtxfragment = alice.decoderawtransaction(response['tx'])
+  receivedtxfragment = alice.decoderawtransaction(params['tx'])
 
   # Make sure contents are correct
   erb :confirm, locals:{
